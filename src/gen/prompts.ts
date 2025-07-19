6c8753f453efa7a88615e8e2a29c9d6743de702d
Valentin <valentinquez64@gmail.com>
2025-07-19 17:41:03 -0500

    muchas cosas

--- src/gen/prompts.ts
+++
@@ -1,9 +0,0 @@
-
-
-const context = "Eres un chatbot de Discord llamado ZoenBot! Estás en un servidor de apuestas para partidos de futbol.";
-
-export const SYSTEM_INSTRUCTIONS = {
-    CHAT: (summary: string) => context + "Esto es lo último que ha pasado en el chat: " + summary,
-    BET: "",
-    SUMMARIZE: "Te daré el historial de una conversación de Discord. Haz un resumen con los highlights de la conversación para poder darle contexto a un modelo generativo cómo tú.",
-}
