2004dc51d64e6eeeecc54b6fa1fee8b91ee322b0
sofia.garcia <sofia.garcia@utec.edu>
2025-07-19 16:20:16 -0500

    gen-ai: chat

--- src/gen/client.ts
+++ src/gen/client.ts
@@ -1,12 +1,17 @@
 import { GoogleGenAI } from "@google/genai";
-const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
+import { GEMINI_API_KEY } from "../config/credentials";
+import { SYSTEM_INSTRUCTIONS } from "./prompts";
 
 const ai = new GoogleGenAI({ apiKey: GEMINI_API_KEY});
 const modelName = "gemini-2.5-flash";
 
-export async function answer(query: string) {
+export async function chat(query: string) {
   const response = await ai.models.generateContent({
     model: modelName,
+    config: {
+      systemInstruction: SYSTEM_INSTRUCTIONS.CHAT,
+      temperature: 1.5,
+    },
     contents: query,
   });
   console.log(response.text);
---
+++ src/gen/prompts.ts
@@ -0,0 +1,9 @@
+
+
+const context = "Eres un chatbot de Discord llamado ZoenBot! Estás en un servidor de apuestas para partidos de futbol.";
+
+export const SYSTEM_INSTRUCTIONS = {
+    CHAT: context,
+    BET: "",
+    SUMMARIZE: "Te daré el historial de una conversación de Discord. Haz un resumen con los highlights de la conversación para poder darle contexto a un modelo generativo cómo tú.",
+}
