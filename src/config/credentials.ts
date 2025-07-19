e5050f772c42d789ce1ea37cc584e2a9c76d6120
Valentin <valentinquez64@gmail.com>
2025-07-19 15:56:41 -0500

    credentials.ts

---
+++ src/config/credentials.ts
@@ -0,0 +1,8 @@
+export const OWNER_ID = process.env.OWNER_ID!;
+export const GENERAL_CHANNEL_ID  = process.env.GENERAL_CHANNEL_ID!;
+export const MONGO_DB_CONNECTION_STRING = process.env.MONGO_DB_CONNECTION_STRING!;
+export const DISCORD_TOKEN = process.env.DISCORD_TOKEN!;
+export const CLIENT_ID = process.env.CLIENT_ID!;
+export const GUILD_ID = process.env.GUILD_ID!;
+export const GEMINI_API_KEY = process.env.GEMINI_API_KEY!;
+export const REQUIRED_ROLE = process.env.REQUIRED_ROLE || "ADMIN";
