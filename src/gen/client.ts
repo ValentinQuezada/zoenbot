4497b757a70d9d75e7a3ad20e5f8cad8cedb6c17
sofia.garcia <sofia.garcia@utec.edu>
2025-07-19 17:01:54 -0500

    gen-ai: summarize + context

--- src/gen/client.ts
+++ src/gen/client.ts
@@ -1,18 +1,34 @@
 import { GoogleGenAI } from "@google/genai";
 import { GEMINI_API_KEY } from "../config/credentials";
 import { SYSTEM_INSTRUCTIONS } from "./prompts";
+import { contextMap } from "../index";
 
 const ai = new GoogleGenAI({ apiKey: GEMINI_API_KEY});
 const modelName = "gemini-2.5-flash";
 
-export async function chat(query: string) {
+export async function chat(query: string, summary: string) {
   const response = await ai.models.generateContent({
     model: modelName,
     config: {
-      systemInstruction: SYSTEM_INSTRUCTIONS.CHAT,
+      systemInstruction: SYSTEM_INSTRUCTIONS.CHAT(summary),
       temperature: 1.5,
     },
-    contents: query,
+    contents: "Te acaban de taguear el siguiente mensaje: " + query,
+  });
+  console.log(response.text);
+}
+
+export async function summarize(channel: string) {
+  const channelContext = contextMap.get(channel)!;
+  const conversation = channelContext.map(m => `${m.author.username}: ${m.content}`).join('\n');
+
+  const response = await ai.models.generateContent({
+    model: modelName,
+    config: {
+      systemInstruction: SYSTEM_INSTRUCTIONS.SUMMARIZE,
+      temperature: 1.5,
+    },
+    contents: conversation,
   });
   console.log(response.text);
 }
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import 'dotenv/config'
-import { Client, GatewayIntentBits, REST, Routes, Interaction } from 'discord.js'
+import { Client, GatewayIntentBits, REST, Routes, Interaction, Message } from 'discord.js'
 import commands from './config/commands'
 import holaCommand from './commands/hola'
 import checkstockCommand from './commands/checkstock'
@@ -13,6 +13,8 @@ const client = new Client({
 })
 
 const rest = new REST({ version: '10' }).setToken(process.env.TOKEN_DISCORD as string)
+
+export const contextMap = new Map<string, Message[]>();
 
 client.once('ready', async () => {
   if (!client.user) return
@@ -37,9 +39,19 @@ client.on('interactionCreate', async (interaction: Interaction) => {
 })
 
 
-client.on('messageCreate', async (message) => {
+
+client.on('messageCreate', async (message: Message) => {
   if (message.author.bot) return;
 
+  // GET CHANNEL CONTEXT
+  const key = message.channel.id;
+  if (!contextMap.has(key)) { contextMap.set(key, []); }
+  
+  const context = contextMap.get(key)!;
+  context.push(message);
+  if (context.length > 15) context.shift();
+
+  // IF TAGGED
   if (client.user && message.mentions.has(client.user.id)) {
     await message.reply(`¡Me has taggeado!: ${message}`);
   }
